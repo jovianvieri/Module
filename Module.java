347451c8313a16f4d863f8f457ea801b2713dd99
jovianvieri <86920623+jovianvieri@users.noreply.github.com>
2021-07-04 22:09:08 +0700

    Add files via upload

---
+++ Module.java
@@ -0,0 +1,5 @@
+public class Module{
+  public static void main(String[] args) {
+    System.out.println("MATA (Manajemen Pengetahuan) adalah sebuah modul yang memiliki fungsi sebagai platform kolaborasi berbasis media sosial");
+  }
+}
